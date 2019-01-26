c26965dc8f31f29c81db974bc0e5f0df21e42803
Harsh Mishra <33103745+Jarvis-8035@users.noreply.github.com>
2019-01-26 05:35:39 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,41 +1,146 @@
 from selenium import webdriver
 import requests
 import bs4
-import os
 import random
+import time
+import speech_recognition as sr
+
+r = sr.Recognizer()
+r.energy_threshold=1000
+r.operation_timeout = 5
+st="next"
+
+def listenn():
+    global st
+    while True:
+        with sr.Microphone() as source :
+            print("Say something!")
+            audio = r.listen( source )
+        try:
+            print(r.recognize_google(audio))
+            strr = r.recognize_google(audio)
+            strr = strr.lower()
+            print(strr)
+            if strr=="play":
+                st="play"
+                break
+            elif strr=="pause":
+                st="pause"
+                break
+            elif strr=="next":
+                st="next"
+                break
+            elif strr=="close":
+                st="close"
+                break
+            else:
+                print("Unrecognised Command !!")
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return
 
 state = 1
 
-def click():
-    global state
-    browser.find_element_by_css_selector('.sc-button-play.playButton.sc-button.m-stretch').click()
+query_url = "https://soundcloud.com/search/sounds?q="
 
-query_url = "https://soundcloud.com/search?q="
-
-#Path for chromeDriver
-browser = webdriver.Chrome("E:\chromedriver.exe")
-browser.get("https://soundcloud.com")
-
-while True:
-    print("Tell your Mood !! ")
-    print(">>> 1 - Sad")
-    print(">>> 2 - Happy")
-    print(">>> 3 - Excited")
-    print(">>> 4 - Depressed")
-    print(">>> 0 - Exit")
-    print()
-    choice = int(input(">>> Your choice: "))
+def play_music(stir):
+    def click():
+        global state
+        browser.find_element_by_css_selector('.sc-button-play.playButton.sc-button.m-stretch').click()
+    choice = 0
+    global st
+    #Path for chromeDriver
+    browser = webdriver.Chrome("E:\chromedriver.exe")
+    browser.get("https://soundcloud.com")
+    if stir.lower()=="sad":
+        choice=1
+    elif stir.lower()=="happy":
+        choice=2
+    elif stir.lower()=="excited":
+        choice=3
+    elif stir.lower()=="depressed":
+        choice=4
+    
     if choice == 0:
         browser.quit()
-        break
     print()
-
     # search according to the mood
     if choice == 1:
         url = query_url + "sad"
         request = requests.get(url)
         soup = bs4.BeautifulSoup(request.text, "lxml")
-
+    
+        tracks = soup.select("h2")[3:]
+        track_links = []
+        track_names = []
+        
+        for index, track in enumerate(tracks):
+            track_links.append(track.a.get("href"))
+            track_names.append(track.text)
+            print()
+        while True:
+            if st=="next":
+                choice = random.randint(0,5)
+                print("Now playing: " + track_names[choice])
+                browser.get("http://soundcloud.com" + track_links[choice])
+                browser.find_element_by_css_selector('.sc-button-play.playButton.sc-button.m-stretch').click()
+                state=1
+                st="play"
+                
+            listenn()
+            print(st+" inside play")
+            if st=="play" and state==0:
+                state=1
+                click()
+            elif st=="pause" and state==1:
+                state=0
+                click()
+            elif st=="close":
+                state=0
+                browser.quit()
+                break
+            time.sleep(1)
+    
+    elif choice == 2:
+        url = query_url + "happy"
+        request = requests.get(url)
+        soup = bs4.BeautifulSoup(request.text, "lxml")
+    
+        tracks = soup.select("h2")[3:]
+        track_links = []
+        track_names = []
+        
+        for index, track in enumerate(tracks):
+            track_links.append(track.a.get("href"))
+            track_names.append(track.text)
+            print()
+        st="next"
+        while True:
+            if st=="next":
+                choice = random.randint(0,5)
+                print("Now playing: " + track_names[choice])
+                browser.get("http://soundcloud.com" + track_links[choice])
+                browser.find_element_by_css_selector('.sc-button-play.playButton.sc-button.m-stretch').click()
+                state=1
+                
+            st = input()
+            if st=="play" and state==0:
+                state=1
+                click()
+            elif st=="pause" and state==1:
+                state=0
+                click()
+            elif st=="close":
+                state=0
+                browser.quit()
+                break
+    elif choice == 3:
+        url = query_url + "excited"
+        request = requests.get(url)
+        soup = bs4.BeautifulSoup(request.text, "lxml")
+        
         tracks = soup.select("h2")[3:]
         track_links = []
         track_names = []
@@ -52,7 +157,7 @@ while True:
                 browser.get("http://soundcloud.com" + track_links[choice])
                 browser.find_element_by_css_selector('.sc-button-play.playButton.sc-button.m-stretch').click()
                 state=1
-
+    
             st = input()
             if st=="play" and state==0:
                 state=1
@@ -62,10 +167,38 @@ while True:
                 click()
             elif st=="close":
                 state=0
+                browser.quit()
                 break
-
-        continue
-
-print()
-print("Goodbye!")
-print()
+    
+    elif choice == 1:
+        url = query_url + "depressed"
+        request = requests.get(url)
+        soup = bs4.BeautifulSoup(request.text, "lxml")
+    
+        tracks = soup.select("h2")[3:]
+        track_links = []
+        track_names = []
+        
+        for index, track in enumerate(tracks):
+            track_links.append(track.a.get("href"))
+            track_names.append(track.text)
+            print()
+        st="next"
+        while True:
+            if st=="next":
+                choice = random.randint(0,5)
+                print("Now playing: " + track_names[choice])
+                browser.get("http://soundcloud.com" + track_links[choice])
+                browser.find_element_by_css_selector('.sc-button-play.playButton.sc-button.m-stretch').click()
+                state=1
+            st = input()
+            if st=="play" and state==0:
+                state=1
+                click()
+            elif st=="pause" and state==1:
+                state=0
+                click()
+            elif st=="close":
+                state=0
+                browser.quit()
+                break
