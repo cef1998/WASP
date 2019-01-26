f44a1c9d285df5a7b8d73b67fd7ac1e171ba4fdc
Harsh Mishra <33103745+Jarvis-8035@users.noreply.github.com>
2019-01-26 18:28:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import speech_recognition as sr
-import os
 import files_folders_open as ffo
-import organizebytype as oft
 import music
 import utilities
+import google_surf
 
 r = sr.Recognizer()
-r.energy_threshold=2000
+r.energy_threshold=2500
 r.operation_timeout = 2
+fil_name = "nothisdfdfs"
 
 while True:
     with sr.Microphone() as source :
@@ -24,82 +24,16 @@ while True:
             return li
              
         li = get_words(str)
-        
-        def opening(li):
-            stri = ""
-            open_flag = 0
-            file_flag = 0
-            drive_flag =  0
-            folder_flag = 0
-           
-            for st in li:
-                if st=="open":
-                   open_flag = 1
-                elif st=="drive":
-                   drive_flag = 1
-                   stri = li[1]
-                elif st=="folder":
-                    folder_flag = 1
-                elif st=="file":
-                    file_flag=1
-                
-                if len(li) == 0:
-                    print("Invalid command")
-                    break
-                elif(open_flag==1 and drive_flag==1):
-                    ffo.open_drive(stri)
-                    break
-                elif(open_flag==1 and folder_flag==1):
-                    li = li[2:]
-                    li = "".join(li).lower()
-                    file_list = os.listdir(ffo.path)
-                    nw_li=[]
-                    for i in file_list:
-                        tmp = i.split(" ")
-                        nw_li.append("".join(tmp).lower())
-                    if li in nw_li:
-                        stri = file_list[nw_li.index(li)]
-                    else :
-                        break
-                    ffo.open_folder(stri)
-                    break
-                elif(open_flag==1 and file_flag==1):
-                    sz = len(li)
-                    li_n = li[2:sz-1]
-                    li_n = "".join(li_n).lower()
-                    li_n+='.'
-                    li_n+=li[-1]
-                    li_n = li_n.lower()
-                    print(ffo.path)
-                    file_list = os.listdir(ffo.path)
-                    nw_li=[]
-                    for i in file_list:
-                        tmp = i.split(" ")
-                        nw_li.append("".join(tmp).lower())
-                    print(li_n)
-                    if li_n in nw_li:
-                        stri = file_list[nw_li.index(li_n)]
-                    else :
-                        break
-                    ffo.open_file(stri)
-                    break
-                elif(str == "organize" or str == "organised"):
-                    oft.organize(ffo.path)
-                    break
-                elif (str == "go back"):
-                    ffo.close_current_folder()
-                    break
-                elif (str == "close"):
-                    ffo.close_this()
-                    break
-                else:
-                    continue
+
         if "open" in li or "Open" in li or "close" in li or "back" in li or "organise" in li:
-            opening(li)
+            ffo.opening(li)
         elif ("play" in li or "Play" in li) and ("music" in li or "Music" in li):
+            print("play kro bhai")
             music.play_music(li[1])
         elif "switch" in li:
             utilities.tabs_switching_operation()
+        elif "search" in li and "google" in li:
+            google_surf.search()
         elif "bye" in li:
             break
     except sr.UnknownValueError:
